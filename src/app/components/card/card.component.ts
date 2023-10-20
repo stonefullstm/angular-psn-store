41a28372b854763fed0cefcb5d3cb726c3291680
stonefullstm <stonefull.stm@gmail.com>
2023-10-19 21:16:58 -0300

    Criação de demais componentes

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input()
+  gameCover: string = ""
+  @Input()
+  gameLabel: string = ""
+  @Input()
+  gameType: string = ""
+  @Input()
+  gamePrice: string = ""
   constructor() { }
 
   ngOnInit(): void {
